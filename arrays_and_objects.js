0de3ca5b32625e4f450f61776a80c6e3d43e5059
Muralidharan Vivekananthan <87437063+muralitherun@users.noreply.github.com>
2023-08-06 06:53:27 +0530

    Create arrays_and_objects.js

---
+++ arrays_and_objects.js
@@ -0,0 +1,25 @@
+/*CREATING ARRAYS AND OBJECTS*/
+
+var clothes = [];
+clothes.push("Shirt");
+clothes.push("T-Shirt");
+clothes.push("Blazer");
+clothes.push("Coat");
+clothes.push("Pullover");
+
+clothes.pop();
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "lilac";
+favCar.covertible = true;
+
+console.log(favCar);
+
+/*OUTPUT*/
+
+/*
+Blazer
+{ color: 'lilac', covertible: true }
+*/
